0aa2e28c44798c0eb7874c5d39d86b017eb0d2f1
Dezolutions <mars92851@gmail.com>
2021-11-30 03:00:51 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import { Heading, Layout, Page } from "@shopify/polaris";
+
+const Index = () => {
+  
+  return (
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Heading>Customer metafields</Heading>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+};
+
+export default Index;
